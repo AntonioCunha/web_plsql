adea61ac4f0b5de350d2346a0051d01fa907b2c6
João Martins <joao.martins@sysnovare.pt>
2022-12-29 14:51:04 +0000

    Title: refactor default page code

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,8 @@ function requestHandler(req: express.Request, res: express.Response, databasePoo
 		// should we switch to the default page if there is one defined
 		if (typeof req.params.name !== 'string' || req.params.name.length === 0) {
 			if (typeof options.defaultPage === 'string' && options.defaultPage.length > 0) {
-				const newUrl = url.resolve(req.originalUrl + '/' + options.defaultPage, '');
+				const originalUrl = req.originalUrl.charAt(req.originalUrl.length - 1) === '/' ? req.originalUrl : req.originalUrl + '/';
+				const newUrl = url.resolve(originalUrl + options.defaultPage, '');
 				trace.write(`Redirect to the url "${newUrl}"`);
 				res.redirect(newUrl);
 			} else {
